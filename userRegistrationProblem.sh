09151a95f7902e8aef5fc1224afebb8508b2dac1
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-12 15:43:49 +0530

    [Siva]Added:To check given first name starts with capital and has minimum 3 characters

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,2 +1,11 @@
-echo welcome to userRegistrationProblem
+#!/bin/bash -x
 
+read -p "Enter a First Name: " firstName
+pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
+
+if [[ $firstName =~ $pattern ]]
+then
+	echo "firstName is Valid"
+else
+	echo "firstName is Invalid"
+fi
