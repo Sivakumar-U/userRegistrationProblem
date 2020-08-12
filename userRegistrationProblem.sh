bcdb27b4145a94ed060608b8091e0db1b5cfd24e
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-12 16:08:58 +0530

    [Siva]Added:To check given password contains atleast one numeric number

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter a password : " password
 
-if [[ ${#password} -ge 8 && $password == *[[:upper:]]* ]]
+if [[ ${#password} -ge 8 && $password == *[[:upper:]]* && $password == *[0-9]* ]]
 then
 	echo "password is Valid"
 else
