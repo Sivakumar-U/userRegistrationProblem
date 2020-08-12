c4d61dc9cc9b4d6e5d835e7f3e468bff23fd8766
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-12 15:52:09 +0530

    [Siva]Added:To check the given email is valid or not

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-read -p "Enter a First Name: " lastName
-pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
+read -p "Enter a Email : " email
+pattern="^[a-zA-Z0-9]{3,15}(|[.|_|%|+|-]?[a-zA-Z0-9]+)@[a-zA-Z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})$"
 
-if [[ $lastName =~ $pattern ]]
+if [[ $email =~ $pattern ]]
 then
-	echo "lastName is Valid"
+	echo "email is Valid"
 else
-	echo "lastName is Invalid"
+	echo "email is Invalid"
 fi
