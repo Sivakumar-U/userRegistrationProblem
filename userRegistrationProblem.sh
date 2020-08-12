e13f5d1e72427fc8da87336899c9d0aa9d0c7eb4
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-12 16:13:00 +0530

    [Siva]Added:To check given password contains atleast one special character

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 
 read -p "Enter a password : " password
+pattern=$(($(tr -d '[[:alnum:]]' <<< $password | wc -m)-1))
 
-if [[ ${#password} -ge 8 && $password == *[[:upper:]]* && $password == *[0-9]* ]]
+if [[ ${#password} -ge 8 && $password == *[[:upper:]]* && $password == *[0-9]* && $pattern -eq 1 ]]
 then
 	echo "password is Valid"
 else
